664e6c7e9078b6e92496c84d14215fd3428e9e3f
Alexeas <87394014+Alexeas@users.noreply.github.com>
2021-09-14 15:12:31 +0300

    newsystem

--- js/search.js
+++ js/search.js
@@ -12,63 +12,63 @@ var sum         = 15;
 
 title[1]       = 'Изменение ударения при спряжении существительных. Родительный падеж. Винительный падеж.';
 description[1] = 'Изменение ударения при спряжении существительных в Греческом языке.';
-address[1]     = '../grammar/accentsonnounsdeclension.html';
+address[1]     = 'accentsonnounsdeclension.html';
 
 title[2]       = 'Прилагательные. Склонение прилагательных. Окончение -ος, -υς, -ης, -η, -α, -ια, -ο, -υ, ι, -ικο, -ες. Родительный падеж. Винительный падеж.';
 description[2] = 'Склонение прилагательных в Греческом языке.';
-address[2]     = '../grammar/adjectivesdeclension.html';
+address[2]     = 'adjectivesdeclension.html';
 
 title[3]       = 'Зависимое наклонение несовершенного, совершенного вида и глаголы перфектной формы.';
 description[3] = 'Зависимое наклонение глаголов в греческом языке.';
-address[3]     = '../grammar/dependent_mood.html';
+address[3]     = 'dependent_mood.html';
 
 title[4]       = 'Грамматика. Части речи.';
 description[4] = 'Грамматика Греческого языка.';
-address[4]     = '../grammar/grammar.html';
+address[4]     = 'grammar.html';
 
 title[5]       = 'Повелительное наклонение несовершенного и совершенного вида. Неправильные глаголы. Отрицательная форма повелительного наклонения.';
 description[5] = 'Повелительное наклонение глаголов в греческом языке.';
-address[5]     = '../grammar/imperative_mood.html';
+address[5]     = 'imperative_mood.html';
 
 title[6]       = 'Существительные. Имя существительное. Род, число, склонение. Родительный падеж. Винительный падеж.';
 description[6] = 'Существительные в Греческом языке.';
-address[6]     = '../grammar/nouns.html';
+address[6]     = 'nouns.html';
 
 title[7]       = 'Существительные. Cклонение существительных. Женский род. Окончание -η, -α. Родительный падеж. Винительный падеж.';
 description[7] = 'Cклонение существительных Женского рода в Греческом языке.';
-address[7]     = '../grammar/nounsdeclension_f.html';
+address[7]     = 'nounsdeclension_f.html';
 
 title[8]       = 'Существительные. Cклонение существительных. Мужской род. Окончание -ος, -ης, -ας. Родительный падеж. Винительный падеж.';
 description[8] = 'Cклонение существительных Мужского рода в Греческом языке.';
-address[8]     = '../grammar/nounsdeclension_m.html';
+address[8]     = 'nounsdeclension_m.html';
 
 title[9]       = 'Существительные. Cклонение существительных. Средний род. Окончание -ο, -ι, -μα, -υ. Родительный падеж. Винительный падеж.';
 description[9] = 'Cклонение существительных Среднего рода в Греческом языке.';
-address[9]     = '../grammar/nounsdeclension_n.html';
+address[9]     = 'nounsdeclension_n.html';
 
 title[10]       = 'Глаголы. Прошедшее длительное время. Παρατατικός. Спряжение глаголов.';
 description[10] = 'Прошедшее длительное время Глаголов в греческом языке.';
-address[10]     = '../grammar/time_pastimperfect.html';
+address[10]     = 'time_pastimperfect.html';
 
 title[11]       = 'Глаголы. Прошедшее простое время. Αόριστος. Спряжение глаголов. Аорист.';
 description[11] = 'Прошедшее простое время Глаголов в греческом языке.';
-address[11]     = '../grammar/time_pastsimple.html';
+address[11]     = 'time_pastsimple.html';
 
 title[12]       = 'Глаголы. Настоящее время. Спряжение глаголов.';
 description[12] = 'Настоящее время Глаголов в греческом языке.';
-address[12]     = '../grammar/time_present.html';
+address[12]     = 'time_present.html';
 
 title[13]       = 'Глаголы. Будущее время. Спряжение глаголов инфекта. Будущее простое совершенное и несовершенное время.';
 description[13] = 'Будущие времена Инфекта в греческом языке.';
-address[13]     = '../grammar/times_futureinfect.html';
+address[13]     = 'times_futureinfect.html';
 
 title[14]       = 'Глаголы. Настоящее, прошедшее и будущее время. Спряжение глаголов перфекта. Прошедшее сложное совершенное время. Предпрошедшее время. Будущее сложное совершенное время.';
 description[14] = 'Перфектная группа времён Глаголов в греческом языке.';
-address[14]     = '../grammar/times_perfect.html';
+address[14]     = 'times_perfect.html';
 
 title[15]       = 'Глаголы. Время, наклонение. Пассивный и активный залог глаголов. Особые Формы Глагола.';
 description[15] = 'Глаголы в греческом языке.';
-address[15]     = '../grammar/verbs.html';
+address[15]     = 'verbs.html';
 
 function contains(sub, s) {
     var strlen1 = sub.length;
@@ -80,6 +80,7 @@ function contains(sub, s) {
         if(comp == sub) {
             istrue = true;
             break;
+            return false;
         }
     }
 
