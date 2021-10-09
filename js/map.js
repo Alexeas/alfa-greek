6fe71e8e5f739093b1fbb5223d91761b7f2df008
Alexeas <87394014+Alexeas@users.noreply.github.com>
2021-10-10 02:16:15 +0300

    bhj

---
+++ js/map.js
@@ -0,0 +1,31 @@
+function attica() {
+    document.getElementById('image').src = 'images/geography/attica.png';
+};
+
+function macedonia_and_thrace() {
+    document.getElementById('image').src = 'images/geography/macedonia_and_thrace.png';
+};
+
+function epirus_and_wm() {
+    document.getElementById('image').src = 'images/geography/epirus_and_wm.png';
+};
+
+function thessaly_and_cg() {
+    document.getElementById('image').src = 'images/geography/thessaly_and_cg.png';
+};
+
+function peloponnese_wg_ii() {
+    document.getElementById('image').src = 'images/geography/peloponnese_wg_ii.png';
+};
+
+function aegean_islands() {
+    document.getElementById('image').src = 'images/geography/aegean_islands.png';
+};
+
+function crete() {
+    document.getElementById('image').src = 'images/geography/crete.png';
+};
+
+function ams_of_the_holy_mountain() {
+    document.getElementById('image').src = 'images/geography/ams_of_the_holy_mountain.png';
+};
