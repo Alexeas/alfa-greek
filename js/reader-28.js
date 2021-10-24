869dbb0ace4de45691c5c477d476414f8e440279
Alexeas <87394014+Alexeas@users.noreply.github.com>
2021-10-24 17:59:30 +0300

    gamee

---
+++ js/reader-28.js
@@ -0,0 +1,364 @@
+var p_1 = document.getElementById("p_1").textContent;
+var pt_1 = document.getElementById("pt_1").textContent;
+$('#p_1').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_1);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_1);
+    $(this).data('status', false);
+  }
+});
+
+var p_2 = document.getElementById("p_2").textContent;
+var pt_2 = document.getElementById("pt_2").textContent;
+$('#p_2').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_2);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_2);
+    $(this).data('status', false);
+  }
+});
+
+var p_3 = document.getElementById("p_3").textContent;
+var pt_3 = document.getElementById("pt_3").textContent;
+$('#p_3').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_3);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_3);
+    $(this).data('status', false);
+  }
+});
+
+var p_4 = document.getElementById("p_4").textContent;
+var pt_4 = document.getElementById("pt_4").textContent;
+$('#p_4').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_4);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_4);
+    $(this).data('status', false);
+  }
+});
+
+var p_5 = document.getElementById("p_5").textContent;
+var pt_5 = document.getElementById("pt_5").textContent;
+$('#p_5').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_5);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_5);
+    $(this).data('status', false);
+  }
+});
+
+var p_6 = document.getElementById("p_6").textContent;
+var pt_6 = document.getElementById("pt_6").textContent;
+$('#p_6').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_6);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_6);
+    $(this).data('status', false);
+  }
+});
+
+
+var p_7 = document.getElementById("p_7").textContent;
+var pt_7 = document.getElementById("pt_7").textContent;
+$('#p_7').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_7);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_7);
+    $(this).data('status', false);
+  }
+});
+
+var p_8 = document.getElementById("p_8").textContent;
+var pt_8 = document.getElementById("pt_8").textContent;
+$('#p_8').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_8);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_8);
+    $(this).data('status', false);
+  }
+});
+
+var p_9 = document.getElementById("p_9").textContent;
+var pt_9 = document.getElementById("pt_9").textContent;
+$('#p_9').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_9);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_9);
+    $(this).data('status', false);
+  }
+});
+
+var p_10 = document.getElementById("p_10").textContent;
+var pt_10 = document.getElementById("pt_10").textContent;
+$('#p_10').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_10);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_10);
+    $(this).data('status', false);
+  }
+});
+
+var p_11 = document.getElementById("p_11").textContent;
+var pt_11 = document.getElementById("pt_11").textContent;
+$('#p_11').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_11);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_11);
+    $(this).data('status', false);
+  }
+});
+
+var p_12 = document.getElementById("p_12").textContent;
+var pt_12 = document.getElementById("pt_12").textContent;
+$('#p_12').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_12);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_12);
+    $(this).data('status', false);
+  }
+});
+
+var p_13 = document.getElementById("p_13").textContent;
+var pt_13 = document.getElementById("pt_13").textContent;
+$('#p_13').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_13);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_13);
+    $(this).data('status', false);
+  }
+});
+
+var p_14 = document.getElementById("p_14").textContent;
+var pt_14 = document.getElementById("pt_14").textContent;
+$('#p_14').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_14);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_14);
+    $(this).data('status', false);
+  }
+});
+
+var p_15 = document.getElementById("p_15").textContent;
+var pt_15 = document.getElementById("pt_15").textContent;
+$('#p_15').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_15);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_15);
+    $(this).data('status', false);
+  }
+});
+
+var p_16 = document.getElementById("p_16").textContent;
+var pt_16 = document.getElementById("pt_16").textContent;
+$('#p_16').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_16);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_16);
+    $(this).data('status', false);
+  }
+});
+
+var p_17 = document.getElementById("p_17").textContent;
+var pt_17 = document.getElementById("pt_17").textContent;
+$('#p_17').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_17);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_17);
+    $(this).data('status', false);
+  }
+});
+
+var p_18 = document.getElementById("p_18").textContent;
+var pt_18 = document.getElementById("pt_18").textContent;
+$('#p_18').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_18);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_18);
+    $(this).data('status', false);
+  }
+});
+
+var p_19 = document.getElementById("p_19").textContent;
+var pt_19 = document.getElementById("pt_19").textContent;
+$('#p_19').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_19);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_19);
+    $(this).data('status', false);
+  }
+});
+
+var p_20 = document.getElementById("p_20").textContent;
+var pt_20 = document.getElementById("pt_20").textContent;
+$('#p_20').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_20);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_20);
+    $(this).data('status', false);
+  }
+});
+
+var p_21 = document.getElementById("p_21").textContent;
+var pt_21 = document.getElementById("pt_21").textContent;
+$('#p_21').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_21);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_21);
+    $(this).data('status', false);
+  }
+});
+
+var p_22 = document.getElementById("p_22").textContent;
+var pt_22 = document.getElementById("pt_22").textContent;
+$('#p_22').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_22);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_22);
+    $(this).data('status', false);
+  }
+});
+
+var p_23 = document.getElementById("p_23").textContent;
+var pt_23 = document.getElementById("pt_23").textContent;
+$('#p_23').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_23);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_23);
+    $(this).data('status', false);
+  }
+});
+
+var p_24 = document.getElementById("p_24").textContent;
+var pt_24 = document.getElementById("pt_24").textContent;
+$('#p_24').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_24);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_24);
+    $(this).data('status', false);
+  }
+});
+
+var p_25 = document.getElementById("p_25").textContent;
+var pt_25 = document.getElementById("pt_25").textContent;
+$('#p_25').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_25);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_25);
+    $(this).data('status', false);
+  }
+});
+
+var p_26 = document.getElementById("p_26").textContent;
+var pt_26 = document.getElementById("pt_26").textContent;
+$('#p_26').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_26);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_26);
+    $(this).data('status', false);
+  }
+});
+
+var p_27 = document.getElementById("p_27").textContent;
+var pt_27 = document.getElementById("pt_27").textContent;
+$('#p_27').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_27);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_27);
+    $(this).data('status', false);
+  }
+});
+
+var p_28 = document.getElementById("p_28").textContent;
+var pt_28 = document.getElementById("pt_28").textContent;
+$('#p_28').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_28);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_28);
+    $(this).data('status', false);
+  }
+});
---
+++ js/reader-9.js
@@ -0,0 +1,117 @@
+var p_1 = document.getElementById("p_1").textContent;
+var pt_1 = document.getElementById("pt_1").textContent;
+$('#p_1').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_1);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_1);
+    $(this).data('status', false);
+  }
+});
+
+var p_2 = document.getElementById("p_2").textContent;
+var pt_2 = document.getElementById("pt_2").textContent;
+$('#p_2').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_2);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_2);
+    $(this).data('status', false);
+  }
+});
+
+var p_3 = document.getElementById("p_3").textContent;
+var pt_3 = document.getElementById("pt_3").textContent;
+$('#p_3').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_3);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_3);
+    $(this).data('status', false);
+  }
+});
+
+var p_4 = document.getElementById("p_4").textContent;
+var pt_4 = document.getElementById("pt_4").textContent;
+$('#p_4').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_4);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_4);
+    $(this).data('status', false);
+  }
+});
+
+var p_5 = document.getElementById("p_5").textContent;
+var pt_5 = document.getElementById("pt_5").textContent;
+$('#p_5').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_5);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_5);
+    $(this).data('status', false);
+  }
+});
+
+var p_6 = document.getElementById("p_6").textContent;
+var pt_6 = document.getElementById("pt_6").textContent;
+$('#p_6').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_6);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_6);
+    $(this).data('status', false);
+  }
+});
+
+
+var p_7 = document.getElementById("p_7").textContent;
+var pt_7 = document.getElementById("pt_7").textContent;
+$('#p_7').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_7);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_7);
+    $(this).data('status', false);
+  }
+});
+
+var p_8 = document.getElementById("p_8").textContent;
+var pt_8 = document.getElementById("pt_8").textContent;
+$('#p_8').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_8);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_8);
+    $(this).data('status', false);
+  }
+});
+
+var p_9 = document.getElementById("p_9").textContent;
+var pt_9 = document.getElementById("pt_9").textContent;
+$('#p_9').click(function(){
+    if (!$(this).data('status')) {
+    $(this).html(pt_9);
+    $(this).data('status', true);
+  }
+  else {
+    $(this).html(p_9);
+    $(this).data('status', false);
+  }
+});
