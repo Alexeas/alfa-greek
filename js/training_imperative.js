2ec449cfe5ba2fbac238c349b497e1a739609903
Alexeas <87394014+Alexeas@users.noreply.github.com>
2022-02-12 22:17:07 +0300

    qwerty

---
+++ js/training_imperative.js
@@ -0,0 +1,297 @@
+const q_1 = "Ты говори (λέω)";
+const q_2 = "Вы говорите (λέω)";
+const q_3 = "Ты пей (πίνω)";
+const q_4 = "Вы пейте (πίνω)";
+const q_5 = "Ты ешь (τρώω)";
+const q_6 = "Вы ешьте (τρώω)";
+const q_7 = "Ты бери (παίρνω)";
+const q_8 = "Вы берите (παίρνω)";
+const q_9 = "Ты давай (δίνω)";
+const q_10 = "Вы давайте (δίνω)";
+const q_11 = "Ты поднимайся (ανεβαίνω)";
+const q_12 = "Вы поднимайтесь (ανεβαίνω)";
+const q_13 = "Ты спускайся (κατεβαίνω)";
+const q_14 = "Вы спускайтесь (κατεβαίνω)";
+const q_15 = "Ты входи (μπαίνω)";
+const q_16 = "Вы входите (μπαίνω)";
+const q_17 = "Ты выходи (βγαίνω)";
+const q_18 = "Вы выходите (βγαίνω)";
+const q_19 = "Ты уходи (φεύγω)";
+const q_20 = "Вы уходите (φεύγω)";
+const q_21 = "Ты знай (ξέρω)";
+const q_22 = "Вы знайте (ξέρω)";
+const q_23 = "Ты желай (θέλω)";
+const q_24 = "Вы желайте (θέλω)";
+const q_25 = "Ты имей (έχω)";
+const q_26 = "Вы имейте (έχω)";
+const q_27 = "Ты заговори (μιλάω)";
+const q_28 = "Вы заговорите (μιλάω)";
+const q_29 = "Ты делай (κάνω)";
+const q_30 = "Вы делайте (κάνω)";
+const q_31 = "Ты покупай (αγοράζω)";
+const q_32 = "Вы покупайте (αγοράζω)";
+const q_33 = "Ты смотри (βλέπω)";
+const q_34 = "Вы смотрите (βλέπω)";
+const q_35 = "Ты понимай (καταλαβαίνω)";
+const q_36 = "Вы понимайте (καταλαβαίνω)";
+const q_37 = "Ты работай (δουλεύω)";
+const q_38 = "Вы работайте (δουλεύω)";
+const q_39 = "Ты мой (πλένω)";
+const q_40 = "Вы мойте (πλένω)";
+const q_41 = "Ты одевайся (ντύνω)";
+const q_42 = "Вы одевайтесь (ντύνω)";
+const q_43 = "Ты закрывай (κλείνω)";
+const q_44 = "Вы закрывайте (κλείνω)";
+const q_45 = "Ты смейся (γελάω)";
+const q_46 = "Вы смейтесь (γελάω)";
+const q_47 = "Ты слушай (ακούω)";
+const q_48 = "Вы слушайте (ακούω)";
+const q_49 = "Ты плачь (κλαίω)";
+const q_50 = "Вы плачьте (κλαίω)";
+const q_51 = "Ты спрашивай (ρωτάω)";
+const q_52 = "Вы спрашивайте (ρωτάω)";
+const q_53 = "Ты отвечай (απαντάω)";
+const q_54 = "Вы отвечайте (απαντάω)";
+const q_55 = "Ты говори (μιλάω)";
+const q_56 = "Вы говорите (μιλάω)";
+
+const q_57 = "Ты скажи (λέω)";
+const q_58 = "Вы скажите (λέω)";
+const q_59 = "Ты попей (πίνω)";
+const q_60 = "Вы попейте (πίνω)";
+const q_61 = "Ты поешь (τρώω)";
+const q_62 = "Вы поешьте (τρώω)";
+const q_63 = "Ты возьми (παίρνω)";
+const q_64 = "Вы возьмите (παίρνω)";
+const q_65 = "Ты дай (δίνω)";
+const q_66 = "Вы дайте (δίνω)";
+const q_67 = "Ты поднимись (ανεβαίνω)";
+const q_68 = "Вы поднимитесь (ανεβαίνω)";
+const q_69 = "Ты спустись (κατεβαίνω)";
+const q_70 = "Вы спуститесь (κατεβαίνω)";
+const q_71 = "Ты войди (μπαίνω)";
+const q_72 = "Вы войдите (μπαίνω)";
+const q_73 = "Ты выйди (βγαίνω)";
+const q_74 = "Вы выйдите (βγαίνω)";
+const q_75 = "Ты уйди (φεύγω)";
+const q_76 = "Вы уйдите (φεύγω)";
+const q_77 = "Ты пожелай (θέλω)";
+const q_78 = "Вы пожелайте (θέλω)";
+const q_79 = "Ты сделай (κάνω)";
+const q_80 = "Вы сделайте (κάνω)";
+const q_81 = "Ты купи (αγοράζω)";
+const q_82 = "Вы купите (αγοράζω)";
+const q_83 = "Ты посмотри (βλέπω)";
+const q_84 = "Вы посмотрите (βλέπω)";
+const q_85 = "Ты пойми (καταλαβαίνω)";
+const q_86 = "Вы поймите (καταλαβαίνω)";
+const q_87 = "Ты сработай (δουλεύω)";
+const q_88 = "Вы сработайте (δουλεύω)";
+const q_89 = "Ты помой (πλένω)";
+const q_90 = "Вы помойте (πλένω)";
+const q_91 = "Ты одень (ντύνω)";
+const q_92 = "Вы оденьте (ντύνω)";
+const q_93 = "Ты закрой (κλείνω)";
+const q_94 = "Вы закройте (κλείνω)";
+const q_95 = "Ты засмейся (γελάω)";
+const q_96 = "Вы засмейтесь (γελάω)";
+const q_97 = "Ты послушай (ακούω)";
+const q_98 = "Вы послушайте (ακούω)";
+const q_99 = "Ты заплачь (κλαίω)";
+const q_100 = "Вы заплачьте (κλαίω)";
+const q_101 = "Ты спроси (ρωτάω)";
+const q_102 = "Вы спросите (ρωτάω)";
+const q_103 = "Ты ответь (απαντάω)";
+const q_104 = "Вы ответьте (απαντάω)";
+
+const ans_1 = "λέγε";
+const ans_2 = "λέγετε";
+const ans_3 = "πίνε";
+const ans_4 = "πίνετε";
+const ans_5 = "τρώγε";
+const ans_6 = "(τρώτε) τρώγετε";
+const ans_7 = "παίρνε";
+const ans_8 = "παίρνετε";
+const ans_9 = "δίνε";
+const ans_10 = "δίνετε";
+const ans_11 = "ανέβαινε";
+const ans_12 = "ανεβαίνετε";
+const ans_13 = "κατέβαινε";
+const ans_14 = "κατεβαίνετε";
+const ans_15 = "μπαίνε (έμπαινε)";
+const ans_16 = "μπαίνετε";
+const ans_17 = "βγαίνε";
+const ans_18 = "βγαίνετε";
+const ans_19 = "φεύγε";
+const ans_20 = "φεύγετε";
+const ans_21 = "ξέρε";
+const ans_22 = "ξέρετε";
+const ans_23 = "θέλε";
+const ans_24 = "θέλετε";
+const ans_25 = "έχε";
+const ans_26 = "έχετε";
+const ans_27 = "μίλησε";
+const ans_28 = "μιλήστε";
+const ans_29 = "κάνε";
+const ans_30 = "κάνετε";
+const ans_31 = "αγόραζε";
+const ans_32 = "αγοράζετε";
+const ans_33 = "βλέπε";
+const ans_34 = "βλέπετε";
+const ans_35 = "καταλάβαινε";
+const ans_36 = "καταλαβαίνετε";
+const ans_37 = "δούλευε";
+const ans_38 = "δουλεύετε";
+const ans_39 = "πλένε";
+const ans_40 = "πλένετε";
+const ans_41 = "ντύνε";
+const ans_42 = "ντύνετε";
+const ans_43 = "κλείνε";
+const ans_44 = "κλείνετε";
+const ans_45 = "γέλα (γέλαγε)";
+const ans_46 = "γελάτε";
+const ans_47 = "άκου (άκουγε)";
+const ans_48 = "ακούετε";
+const ans_49 = "κλαίγε";
+const ans_50 = "κλαίγετε";
+const ans_51 = "ρώτα (ρώταγε)";
+const ans_52 = "ρωτάτε";
+const ans_53 = "απάντα (απάνταγε)";
+const ans_54 = "απαντάτε";
+const ans_55 = "μίλα (μίλαγε)";
+const ans_56 = "μιλάτε (μιλείτε)";
+
+const ans_57 = "πες";
+const ans_58 = "πείτε (πέστε)";
+const ans_59 = "πιες";
+const ans_60 = "πιείτε";
+const ans_61 = "φάε";
+const ans_62 = "φάτε";
+const ans_63 = "πάρε";
+const ans_64 = "πάρτε";
+const ans_65 = "δώσε";
+const ans_66 = "δώστε";
+const ans_67 = "ανέβα";
+const ans_68 = "ανεβείτε";
+const ans_69 = "κατέβα";
+const ans_70 = "κατεβείτε";
+const ans_71 = "μπες (έμπα)";
+const ans_72 = "μπείτε";
+const ans_73 = "βγες (έβγα)";
+const ans_74 = "βγείτε";
+const ans_75 = "φύγε";
+const ans_76 = "φύγτε";
+const ans_77 = "θέλησε";
+const ans_78 = "θελήστε";
+const ans_79 = "κάνε (κάμε)";
+const ans_80 = "κάντε (κάμετε)";
+const ans_81 = "αγόρασε";
+const ans_82 = "αγοράστε";
+const ans_83 = "δες (ιδές)";
+const ans_84 = "δείτε";
+const ans_85 = "κατάλαβε";
+const ans_86 = "καταλάβετε";
+const ans_87 = "δούλεψε";
+const ans_88 = "δουλέψτε (δουλεύτε)";
+const ans_89 = "πλύνε";
+const ans_90 = "πλύντε";
+const ans_91 = "ντύσε";
+const ans_92 = "ντύστε";
+const ans_93 = "κλείσε";
+const ans_94 = "κλείστε";
+const ans_95 = "γέλασε";
+const ans_96 = "γελάστε";
+const ans_97 = "άκου (άκουσε)";
+const ans_98 = "ακούστε";
+const ans_99 = "κλάψε";
+const ans_100 = "κλάψτε";
+const ans_101 = "ρώτησε";
+const ans_102 = "ρωτήστε";
+const ans_103 = "απάντησε";
+const ans_104 = "απαντήστε";
+
+const qw_array = [q_1,q_2,q_3,q_4,q_5,q_6,q_7,q_8,q_9,q_10,q_11,q_12,q_13,q_14,q_15,q_16,q_17,q_18,q_19,q_20,q_21,q_22,q_23,q_24,q_25,q_26,q_27,q_28,q_29,q_30,q_31,q_32,q_33,q_34,q_35,q_36,q_37,q_38,q_39,q_40,
+    q_41,q_42,q_43,q_44,q_45,q_46,q_47,q_48,q_49,q_50,q_51,q_52,q_53,q_54,q_55,q_56,q_57,q_58,q_59,q_60,q_61,q_62,q_63,q_64,q_65,q_66,q_67,q_68,q_69,q_70,q_71,q_72,q_73,q_74,q_75,q_76,q_77,q_78,q_79,q_80,
+    q_81,q_82,q_83,q_84,q_85,q_86,q_87,q_88,q_89,q_90,q_91,q_92,q_93,q_94,q_95,q_96,q_97,q_98,q_99,q_100,q_101,q_102,q_103,q_104];
+const ans_array = [ans_1,ans_2,ans_3,ans_4,ans_5,ans_6,ans_7,ans_8,ans_9,ans_10,ans_11,ans_12,ans_13,ans_14,ans_15,ans_16,ans_17,ans_18,ans_19,ans_20,ans_21,ans_22,ans_23,ans_24,ans_25,ans_26,ans_27,ans_28,ans_29,ans_30,ans_31,ans_32,ans_33,ans_34,ans_35,ans_36,ans_37,ans_38,ans_39,ans_40,
+    ans_41,ans_42,ans_43,ans_44,ans_45,ans_46,ans_47,ans_48,ans_49,ans_50,ans_51,ans_52,ans_53,ans_54,ans_55,ans_56,ans_57,ans_58,ans_59,ans_60,ans_61,ans_62,ans_63,ans_64,ans_65,ans_66,ans_67,ans_68,ans_69,ans_70,ans_71,ans_72,ans_73,ans_74,ans_75,ans_76,ans_77,ans_78,ans_79,ans_80,
+    ans_81,ans_82,ans_83,ans_84,ans_85,ans_86,ans_87,ans_88,ans_89,ans_90,ans_91,ans_92,ans_93,ans_94,ans_95,ans_96,ans_97,ans_98,ans_99,ans_100,ans_101,ans_102,ans_103,ans_104];
+
+    for (var i = 0 ; (i < 104) && (i < qw_array.length) ; i++) {
+        var r = Math.floor(Math.random() * (qw_array.length - i)) + i;
+        var question = qw_array[r];
+        qw_array[r] = qw_array[i];
+        qw_array[i] = question;
+
+        var answer = ans_array[r];
+        ans_array[r] = ans_array[i];
+        ans_array[i] = answer;
+    };
+
+    let score = 104;
+    document.getElementById('question_text').innerHTML = qw_array[0];
+    document.getElementById('score').innerHTML = score;
+    const audio_bad = new Audio('music/bad.wav');
+    const audio_great = new Audio('music/great.wav');
+    let input = document.getElementById('input_verb');
+    const audio_hint = new Audio('music/hint.wav');
+    let block_button = document.getElementById('hint_botton');
+
+                $('#form_game').submit(function() {
+                    $("#input_button").click();
+                    return false;
+                });
+
+                function game_hint() {
+                        audio_hint.play();
+                        input.value = ans_array[0];
+                        table_on();
+                        illum();
+                };
+
+    //Конец Игры
+    function game_over(){
+            document.getElementById('question_text').innerHTML = "Вы завершили тренировку! Начните заново.";
+    };
+    //Главная Функция
+    function game_on_click() {
+        if (qw_array.length == 1) {
+            audio_great.play();
+            reset_button();
+            game_over();
+            score -= 1;
+            document.getElementById('score').innerHTML = score;
+            document.getElementById("input_verb").value = "";
+        }
+        else if (ans_array[0].includes(input.value.toLowerCase()) == true && input.value.length > 2) {
+            audio_great.play();
+            ans_array.shift();
+            qw_array.shift();
+            score -= 1;
+            block_button.setAttribute("onclick", "game_hint()");
+            document.getElementById("input_verb").value = "";
+            document.getElementById('question_text').innerHTML = qw_array[0];
+            document.getElementById('score').innerHTML = score;
+            alert(access);
+        }
+        else {
+            audio_bad.play();
+            document.getElementById('question_text').innerHTML = "Подумайте ещё... " + qw_array[0];
+        }
+    };
+
+    //ПОИСК
+
+        //КНОПКА ИЗМЕНЕНИЕ
+        //перезапуск игры
+        function reload_game() {
+            location.reload();
+        };
+
+        const btn_lid = document.getElementById("input_button");
+        const btn_ans = document.getElementById("hint_botton");
+        function reset_button() {
+            btn_lid.innerText = "Ещё!";
+            btn_lid.setAttribute('onclick', 'reload_game()');
+            btn_ans.removeAttribute('onclick');
+        };
